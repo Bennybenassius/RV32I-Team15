5a5acc96d76f7b11c430033cec8806d5123bcf7c
Adrian <adriankhoo6yk@gmail.com>
2023-12-03 16:12:28 +0000

    instruction rom array memory mapping + comments

--- rtl/Instr_mem.sv
+++ rtl/Instr_mem.sv
@@ -1,19 +1,18 @@
 module Instr_mem #(
-    parameter ADDRESS_WIDTH = 5, // total 2**5 number of address locations
-              DATA_WIDTH = 8     // each instruction word is 32 bits long
+    parameter DATA_WIDTH = 8   // byte addressing, each byte contains 8 bits
 )(
     input logic [31:0]   A,
     output logic [31:0]  RD
 );
 
-logic [31:0] word_addr = {A[31:2],2'b0};
-logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
+logic [31:0] addr_word = {A[31:2],2'b0}; // masking
+logic [DATA_WIDTH-1:0] rom_array [32'hBFC00FFF:32'hBFC00000]; // memory mapping
 
 initial begin 
         $display("Lodaing rom.");
         $readmemh("instruction_code.mem", rom_array);
 end;
 
-assign RD = {rom_array[word_addr], rom_array[word_addr+1], rom_array[word_addr+2], rom_array[word_addr+3]}; // concatenate the 4 byte address to make the full word address
+assign RD = {rom_array[addr_word], rom_array[addr_word+1], rom_array[addr_word+2], rom_array[addr_word+3]}; // concatenate the 4 byte address to make the full word address
 
 endmodule
