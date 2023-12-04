d672360bb94aaa111dafa751bbb805058fd27846
DenizzG <denizgoy2003@gmail.com>
2023-12-04 13:55:01 +0000

    Fixed operations to have Or statement, added zero logic output

--- rtl/ALU.sv
+++ rtl/ALU.sv
@@ -1,8 +1,9 @@
 module ALU(
     input   logic    [31:0]     ALUop1,      //operand 1 for ALU
     input   logic    [31:0]     ALUop2,      //operand 2 for ALU
-    input   logic          ALUControl,     //ALU controls (func3)
+    input   logic    [2:0]      ALUControl,     //ALU controls (func3)
     output  logic    [31:0]     ALUResult,      //ALU output
+    output  logic               Zero
 );
 
 assign Zero = ALUout == 0;
@@ -15,7 +16,7 @@ always_comb begin
         3'b011: ALUout = ALUop1 < Aluop2; //set less than unsigned
         3'b100: ALUout = ALUop1 ^ ALUop2; //XOR
         3'b101: ALUout = ALUop1 >> (ALUop2 & 5'b11111); //srl, shift right logical, masked
-        3'b110: ALUout = ALUop1 & ALUop2; //and 
+        3'b110: ALUout = ALUop1 | AlUop2; // Or operation
         3'b111: ALUout = ALUop1 & ALUop2; //AND
     endcase
 end
