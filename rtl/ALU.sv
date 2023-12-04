0e79f2a8b1d2e122615cf253359a122498fa06be
DenizzG <denizgoy2003@gmail.com>
2023-12-04 13:53:57 +0000

    Added all ALU instructions required -Deniz

--- rtl/ALU.sv
+++ rtl/ALU.sv
@@ -1,13 +1,22 @@
 module ALU(
     input   logic    [31:0]     ALUop1,      //operand 1 for ALU
     input   logic    [31:0]     ALUop2,      //operand 2 for ALU
-    input   logic               ALUcrtl,     //ALU controls
-    output  logic    [31:0]     ALUout,      //ALU output
-    output  logic               EQ           //Whether operands are equal
+    input   logic          ALUControl,     //ALU controls (func3)
+    output  logic    [31:0]     ALUResult,      //ALU output
 );
 
-assign ALUout = ALUcrtl ? (ALUop1 + ALUop2) : (ALUop1 - ALUop2);
+assign Zero = ALUout == 0;
 
-assign EQ = (ALUop1 == ALUop2);   //Flag for equals or not
-    
+always_comb begin
+    case(ALUctrl)
+        3'b000: ALUout = ALUop1 + ALUop2; //add
+        3'b001: ALUout = ALUop1 << (ALUop2 & 5'b11111); //sll, shift left logical, 
+        3'b010: ALUout = $signed(ALUop1) < $signed(Aluop2); //set less than signed
+        3'b011: ALUout = ALUop1 < Aluop2; //set less than unsigned
+        3'b100: ALUout = ALUop1 ^ ALUop2; //XOR
+        3'b101: ALUout = ALUop1 >> (ALUop2 & 5'b11111); //srl, shift right logical, masked
+        3'b110: ALUout = ALUop1 & ALUop2; //and 
+        3'b111: ALUout = ALUop1 & ALUop2; //AND
+    endcase
+end
 endmodule
