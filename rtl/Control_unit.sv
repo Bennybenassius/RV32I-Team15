36ca37b6ec411781250c424d5eac940c7b69c07d
Benny <bencom2014@live.com>
2023-12-05 12:18:22 +0000

    Added AND, XOR and SLL

--- rtl/Control_unit.sv
+++ rtl/Control_unit.sv
@@ -1,6 +1,4 @@
-module Control_unit #(
-    parameter D_WIDTH = 32
-)(  
+module Control_unit (  
     //Input
     input logic [6: 0]   op,
     input logic [2: 0]   funct3,
@@ -14,7 +12,7 @@ module Control_unit #(
     output logic [1: 0]         ImmSrc,
     output logic [1: 0]         PCSrc,
     output logic                MemWrite,          //memory write enable
-    output logic                ResultSrc
+    output logic [1: 0]         ResultSrc
 );
 
 always_comb begin
@@ -22,34 +20,43 @@ always_comb begin
         //================================================================
         7'd51   :   begin
             case (funct3)
-
-                3'b000    :   begin 
-                    case(funct7)
-
-                        1'b0    :   begin   // ADD
-                            PCSrc = 2'b0;       // no branching
-                            ResultSrc = 2'b0;   // bypass data mem
-                            MemWrite = 1'b0;    // not writing to data mem
-                            ALUControl = 3'b1;  // addition
-                            ALUSrc = 1'b0;      // register operand
-                            ImmSrc = 2'b0;      // not using imm
-                            RegWrite = 1'b1;    // write to destination reg
-                        end
-
-                        default: begin // do nothing
-                            PCSrc = 2'b0;
-                            ResultSrc = 2'b0;
-                            MemWrite = 1'b0;
-                            ALUControl = 3'b0; 
-                            ALUSrc = 1'b0;
-                            ImmSrc = 2'b0;
-                            RegWrite = 1'b0; 
-                        end
-
-                    endcase
+                3'b000  :   begin   //ADD
+                    PCSrc = 2'b0;       // no branching
+                    ResultSrc = 2'b0;   // bypass data mem
+                    MemWrite = 1'b0;    // not writing to data mem
+                    ALUControl = 3'b0;  // addition
+                    ALUSrc = 1'b0;      // register operand
+                    ImmSrc = 2'b0;      // not using imm
+                    RegWrite = 1'b1;    // write to destination reg
                 end
-
-                default: begin // do nothing
+                3'b001  :   begin   //shift left logical
+                    PCSrc = 2'b0;
+                    ResultSrc = 2'b0;
+                    MemWrite = 1'b0;
+                    ALUControl = 3'b001;    // sll
+                    ALUSrc = 1'b0;          // 2 register operands
+                    ImmSrc = 2'b0;          // don't care
+                    RegWrite = 1'b1;        // write to rd
+                end
+                3'b100  :   begin   //XOR (^)
+                    PCSrc = 2'b0;
+                    ResultSrc = 2'b0;
+                    MemWrite = 1'b0;
+                    ALUControl = 3'b100;    // XOR
+                    ALUSrc = 1'b0;          // 2 register operands
+                    ImmSrc = 2'b0;          // don't care
+                    RegWrite = 1'b1;        // write to rd
+                end
+                3'b111  :   begin   //AND (&)
+                    PCSrc = 2'b0;
+                    ResultSrc = 2'b0;
+                    MemWrite = 1'b0;
+                    ALUControl = 3'b111;    // AND
+                    ALUSrc = 1'b0;          // 2 register operands
+                    ImmSrc = 2'b0;          // don't care
+                    RegWrite = 1'b1;        // write to rd
+                end
+                default: begin      // do nothing
                     PCSrc = 2'b0;
                         ResultSrc = 2'b0;
                         MemWrite = 1'b0;
@@ -58,18 +65,16 @@ always_comb begin
                         ImmSrc = 2'b0;
                         RegWrite = 1'b0; 
                 end
-
             endcase
         end
 
-        7'd19   :   begin
+        7'd19   :   begin   // ADDI
             case (funct3)
-
-                3'b000    :   begin // ADDI
+                3'b000    :   begin 
                     PCSrc = 2'b0;       // no branching
                     ResultSrc = 2'b0;   // bypass data mem
                     MemWrite = 1'b0;    // not writing to data mem
-                    ALUControl = 3'b1;  // addition
+                    ALUControl = 3'b0;  // addition
                     ALUSrc = 1'b1;      // add with imm
                     ImmSrc = 2'b1;      // need to sign extend
                     RegWrite = 1'b1;    // write to destination reg
@@ -88,13 +93,12 @@ always_comb begin
             endcase
         end
 
-        7'd99   :   begin // B-type instr
+        7'd99   :   begin   // B-type instr
             case (funct3)
-
                 3'b000    :  begin  // BEQ
                     ResultSrc = 2'b0;   // doesn't matter
                     MemWrite = 1'b0;    // doesn't matter
-                    ALUControl = 3'b0;
+                    ALUControl = 3'b0;  // doesn't matter
                     ALUSrc = 1'b0;      // not using imm
                     ImmSrc = 2'b1;      // need sign extend
                     RegWrite = 1'b0;    // not writing to any reg
@@ -138,7 +142,7 @@ always_comb begin
             PCSrc = 2'b10;      // jump
             ResultSrc = 2'b10;  // choose PC+4
             MemWrite = 1'b0;
-            ALUControl = 3'b1; 
+            ALUControl = 3'b0;   // need add immediate offset to rs1  
             ALUSrc = 1'b1;      // need imm
             ImmSrc = 2'b1;      // sign ext imm
             RegWrite = 1'b1;    // store return address (PC+4) in rd
@@ -148,39 +152,55 @@ always_comb begin
             PCSrc = 2'b1;       // jump
             ResultSrc = 2'b10;  // choose PC+4
             MemWrite = 1'b0;
-            ALUControl = 3'b0; 
-            ALUSrc = 1'b0;
+            ALUControl = 3'b0;  // add PC with sign extended Imm
+            ALUSrc = 1'b0;      
             ImmSrc = 2'b1;      // sign ext imm (JTA)
             RegWrite = 1'b1;    // store return address (PC+4) in rd
         end
         //================================================================
         //Data control part
 
-        7'd3 :   begin        // load types
+        7'd3 :   begin      // load types
             case (funct3)
                 3'b010 : begin  //lw
                     RegWrite = 1'b1;    // allow reg to be loaded
-                    ALUControl = 1'b1;     // alu mode: add
+                    ALUControl = 3'b1;     // alu mode: add
                     ALUSrc = 1'b1;      // use imm
-                    ImmSrc = 1'b1;      // use signextend
-                    PCSrc = 1'b0;       // no branch
+                    ImmSrc = 2'b1;      // use signextend
+                    PCSrc = 2'b0;       // no branch
                     MemWrite = 1'b0;          // not write to memory
-                    ResultSrc = 1'b1;// load word from mem
-
+                    ResultSrc = 2'b1;// load word from mem
                 end
-
+                default: begin // do nothing
+                    PCSrc = 2'b0;
+                    ResultSrc = 2'b0;
+                    MemWrite = 1'b0;
+                    ALUControl = 3'b0; 
+                    ALUSrc = 1'b0;
+                    ImmSrc = 2'b0;
+                    RegWrite = 1'b0;  
+                end
             endcase
         end
         7'd35 : begin       //store types
             case (funct3)
                 3'b10 :begin    //sw
                     RegWrite = 1'b0;    // not write to reg (but write to mem)
-                    ALUControl = 1'b1;     // alu mode: add
+                    ALUControl = 3'b1;     // alu mode: add
                     ALUSrc = 1'b1;      // use imm
-                    ImmSrc = 1'b1;      // use signextend
-                    PCSrc = 1'b0;       // no branch
+                    ImmSrc = 2'b1;      // use signextend
+                    PCSrc = 2'b0;       // no branch
                     MemWrite = 1'b1;          // write to memory
-                    ResultSrc = 1'b0;
+                    ResultSrc = 2'b0;
+                end
+                default: begin // do nothing
+                    PCSrc = 2'b0;
+                    ResultSrc = 2'b0;
+                    MemWrite = 1'b0;
+                    ALUControl = 3'b0; 
+                    ALUSrc = 1'b0;
+                    ImmSrc = 2'b0;
+                    RegWrite = 1'b0;  
                 end
             endcase
         end
