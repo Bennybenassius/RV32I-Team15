1da91fcae1a443fe283ff7d44183bc47954dbabd
DenizzG <denizgoy2003@gmail.com>
2023-12-06 17:18:52 +0000

    Got all ALU instructions working
    Co-authored-by: adrianyk <adriankhoo6yk@gmail.com>

--- ALU_testing/Assembly_instructions.s
+++ ALU_testing/Assembly_instructions.s
@@ -1,10 +1,10 @@
 addi t1, zero, 10
-
-addi a0, t0, 10          /should return 22/
+addi t0, zero, 12
+addi a0, t0, 12          /*should return 24*/
 addi a0, a0, 1 
-and a0, t0, 10          /and for 12 and 10 is all possible cases, should return 8, 1000/
-xor a0, t0, 10          /*should return 6*/
-sll a0, t0, 1           /should return 24/
+and a0, t0, t1         /*and for 12 and 10 is all possible cases, should return 8, 1000*/
+xor a0, t0, t1          /*should return 6*/
+sll a0, t0, t1           /*should return 3000*/
 
-addi t1, zero, 500   /number is 2^31/
-sll a0, t1, 1           /should return 0  jalr, jal test next/
+addi t1, zero, 500   /*number is 2^31*/
+sll a0, t1, t0           /*should return 0  jalr, jal test next*/
