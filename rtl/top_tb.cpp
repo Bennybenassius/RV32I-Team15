93047764bf91058ebf098c7f59cccb27735ae8ac
Adrian <adriankhoo6yk@gmail.com>
2023-12-08 11:46:30 +0000

    separate sections for F1 program output and reference program output

--- rtl/top_tb.cpp
+++ rtl/top_tb.cpp
@@ -27,7 +27,6 @@ int main(int argc, char **argv, char **env) {
     top->clk = 1;
     top->rst = 1;
 
-
     for (i=0; i<1000; i++) {
         //dump variables into VCD file and toggle clock
         for(clk=0; clk<2; clk++) {
@@ -43,9 +42,16 @@ int main(int argc, char **argv, char **env) {
         // vbdHex(2, (top->a0 >> 4) & 0xF);
         // vbdHex(3, (top->a0 >> 8) & 0xF);
         // vbdHex(4, (top->a0 >> 12) & 0xF);
-        if (i > 100) {
+
+        // F1 program output
+        top->trigger = vbdFlag();
+        vbdBar(top->a0 & 0xff);
+        
+        // reference program output
+        /* if (i > 100) {
             vbdPlot(int (top->a0), 0, 255);
-        }
+        } */
+
         vbdCycle(i);
 
         //end vbuddy
