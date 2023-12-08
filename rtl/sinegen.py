def3a4b7c6cfb1fd6d9c49ceac413b680736025f
Adrian <adriankhoo6yk@gmail.com>
2023-12-08 17:36:43 +0000

    removed redundant sinegen.py and sinerom.mem files

--- rtl/sinegen.py
+++
@@ -1,9 +0,0 @@
-import math
-import string
-f = open("sinerom.mem","w")
-for i in range(256):
-    v = int(math.cos(2*3.1416*i/256)*127+127)
-    s = "00 00 00 {hex:2X}\n"
-    f.write(s.format(hex=v))
-
-f.close()
