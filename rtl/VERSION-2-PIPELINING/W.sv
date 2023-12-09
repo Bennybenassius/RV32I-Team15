277f4c73e1856a65424dcc9aa743e2acf8935592
yueming <rrroooyyywang@outlook.com>
2023-12-10 07:31:05 +0800

    Fix block W port name

--- rtl/VERSION-2-PIPELINING/W.sv
+++ rtl/VERSION-2-PIPELINING/W.sv
@@ -1,19 +1,19 @@
 module W (
     //Input
-    input logic [1: 0] ResultSrc_i,
-    input logic [31: 0] ALUResult_i,
-    input logic [31: 0] ReadData_i,
-    input logic [31: 0] PCPlus4_i,
+    input logic [1: 0] ResultSrcW_i,
+    input logic [31: 0] ALUResultW_i,
+    input logic [31: 0] ReadDataW_i,
+    input logic [31: 0] PCPlus4W_i,
     //Output
-    output logic [31: 0]    Result_o
+    output logic [31: 0]    ResultW_o
 
 );
 
 always_comb begin
-    case (ResultSrc_i) begin
-        2'b00: Result_o = ALUResult_i;
-        2'b01: Result_o = ReadData_i;
-        2'b01: Result_o = PCPlus4_i;
+    case (ResultSrcW_i) begin
+        2'b00: Result_o = ALUResultW_i;
+        2'b01: Result_o = ReadDataW_i;
+        2'b01: Result_o = PCPlus4W_i;
         end
     endcase
     
