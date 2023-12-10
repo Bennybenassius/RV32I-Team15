57aefe3f8d2bb3964526c06beb1dc7c4cd52c7bb
Adrian <adriankhoo6yk@gmail.com>
2023-12-10 00:04:18 +0000

    removed pass-by signals, added case statements for PCSrcE (branch/jump) > > Co-authored-by: rrroooyyywang <rrroooyyywang@outlook.com> > Co-authored-by: Bennybenassius <bencom2014@live.com> > Co-authored-by: DenizzG <denizgoy2003@gmail.com>

--- rtl/VERSION-2-PIPELINING/E.sv
+++ rtl/VERSION-2-PIPELINING/E.sv
@@ -1,68 +1,95 @@
-    module E#(
+module E#(
+    //INPUTS
+    input logic [1:0]     JumpE_i,
+    input logic           BranchE_i,
+    input logic [2:0]     ALUControlE_i,
+    input logic           ALUSrcE_i,
 
-        input logic           RegWriteE_i,
-        input logic [1:0]     ResultSrcE_i,
-        input logic           MemWriteE_i,
-        input logic           JumpE_i,
-        input logic           BranchE_i,
-        input logic [2:0]     ALUControlE_i,
-        input logic           ALUSrcE_i,
+    input logic [31:0]    RD1E_i,
+    input logic [31:0]    RD2E_i,
+    input logic [31:0]    PCE_i,
+    input logic [31:0]    ImmExtE_i,
 
-        input logic [31:0]    RD1E_i,
-        input logic [31:0]    RD2E_i,
-        input logic [31:0]    PCE_i,
-        input logic [11:7]    RdE_i,
-        input logic [31:7]    ImmExtE_i,
-        input logic [31:0]    PCPlus4E_i,
+    //OUTPUTS
+    output logic [31:0]   PCTargetE_o,
+    output logic [31:0]   WriteDataE_o,
+    output logic [31:0]   ALUResultE_o,
 
-        output logic [31:0]   PCTargetE_o,
-        output logic [31:0]   PCPlus4E_o,
-        output logic [31:0]   RdE_o,
-        output logic [31:0]   WriteDataE_o,
-        output logic [31:0]   ALUResultE_o,
+    output logic [1:0]    PCSrcE_o,
+    output logic [1:0]    ZeroE
 
-        output logic          MemWriteE_o,
-        output logic [1:0]    ResultSrcE_o,
-        output logic          RegWriteE_o,
-        output logic          PCSrcE_o,
-        output logic          ZeroE
+);
+//=======================================
+//          WIRE
 
-    );
-    //=======================================
-    //          WIRE
+logic  [31:0]  SrcBE = (ALUSrcE_i) ? ImmExtE_i : RD2E_i; 
 
-    assign logic [31:0]    PCTargetE_o    = PCE_i   + ImmExtE;
-    assign logic [31:0]    WriteDataE_o = RD2E_i;
-    logic        [31:0]    SrcBE        = (ALUSrcE_i) ? ImmExtE_i : RD2E_i; 
-    assign logic           RegWriteE_o = RegWriteE_i;
-    assign logic [1:0]     ResultSrcE_o = ResultSrcE_i;
-    assign logic           MemWriteE_o = MemWriteE_i;
-    assign logic [11:7]    RdE_o = RdE_i;
-    assign logic [31:0]    PCPlus4E_o = PCPlus4E_i;
+assign  PCTargetE_o = PCE_i + ImmExtE_i;
+assign  WriteDataE_o = RD2E_i;
 
-    always_comb begin
-        case({JumpE_i, BranchE_i, ZeroE})
-            3'b000:  PCSrcE_o = 1'b0;
-            3'b001:  PCSrcE_o = 1'b0;
-            3'b010:  PCSrcE_o = 1'b0;
-            3'b011:  PCSrcE_o = 1'b1;
-            3'b100:  PCSrcE_o = 1'b1;
-            3'b101:  PCSrcE_o = 1'b1;
-            3'b110:  PCSrcE_o = 1'b1;
-            3'b111:  PCSrcE_o = 1'b1;
-            default: PCSrcE_o = 1'b0; 
-        endcase
-    end
+always_comb begin
+    case(JumpE_i)
+        2'b00 : begin // BLT, BGE
+            case(BranchE_i)
+                1'b1 : begin
+                    case(ZeroE)
+                        2'b00   : PCSrcE_o = 2'b01;     // branch
+                        2'b01   : PCSrcE_o = 2'b01;     // branch
+                        2'b10   : PCSrcE_o = 2'b01;     // branch
+                        default : PCSrcE_o = 2'b00;     // otherwise, no branch
+                    endcase
+                default : PCSrcE_o = 2'b00;             
+                end
+            endcase 
+        end 
 
-    ALU ALU(
-        //INPUT
-        .SrcAE(RD1E_i),
-        .SrcBE(SrcBE)
-        .ALUControlE(ALUControlE_i)
+        2'b01 : begin // BNE, JAL
+            case(BranchE_i)
+                1'b0 : PCSrcE_o = 2'b01; // JAL          // jump (JTA)
 
-        //OUTPUTS
-        .ALUResultE(ALUResultE_o),
-        .ZeroE(ZeroE),
-    );
+                1'b1 : begin // BNE
+                    case(ZeroE)
+                        2'b01   : PCSrcE_o = 2'b00;      // no branch
+                        default : PCSrcE_o = 2'b01;      // otherwise, branch
+                    endcase
+                end
+                default : PCSrcE_o = 2'b00;
+            endcase
+        end
 
-    endmodule
+        2'b10 : begin // JALR
+            case(BranchE_i)
+                1'b0 : PCSrcE_o = 2'b10;                 // jump (rsl + SignExt(imm)) 
+
+                default : PCSrcE_o = 2'b00;             
+            endcase
+        end
+
+        2'b11 : begin // BEQ
+            case(BranchE_i)
+                1'b1 : begin
+                    case(ZeroE)
+                        2'b01   : PCSrcE_o = 2'b01;      // branch
+                        default : PCSrcE_o = 2'b00;      // otherwise, no branch
+                    endcase
+                end
+                default : PCSrcE_o = 2'b00;
+            endcase
+        end
+
+        default : PCSrcE_o = 2'b0;
+    endcase
+end
+
+ALU ALU(
+    //INPUTS
+    .SrcAE(RD1E_i),
+    .SrcBE(SrcBE)
+    .ALUControlE(ALUControlE_i)
+
+    //OUTPUTS
+    .ALUResultE(ALUResultE_o),
+    .ZeroE(ZeroE),
+);
+
+endmodule
