7fb46f7dae1d08b22ad08b5c6ffa6bdd188a9711
Adrian <adriankhoo6yk@gmail.com>
2023-12-11 15:22:40 +0000

    Added stall and flush to hazard unit
    Co-authored-by: Bennybenassius <bencom2014@live.com>

--- rtl/VERSION-3-HAZARDDETECTION/Hazard.sv
+++ rtl/VERSION-3-HAZARDDETECTION/Hazard.sv
@@ -6,7 +6,7 @@ module Hazard(
     input logic [4:0]       Rs2E_i_eh,
     input logic [4:0]       RdE_i_eh,
     input logic [1:0]       PCSrcE_i_eh,
-    input logic             ResultSrcE_i_eh,
+    input logic             ResultSrcE_i_eh, // LSB of ResultSrcE_o (if reading from data_mem)
     input logic [4:0]       RdM_i_mh,
     input logic             RegWriteM_i_mh,
     input logic [4:0]       RdW_i_wh,
@@ -20,6 +20,27 @@ module Hazard(
     output logic [1:0]      ForwardBE_o
 );
 
+//           FORWARDING
 
+
+//              STALL
+//====================================
+logic       should_we_stall;
+
+assign should_we_stall = ResultSrcE_i_eh & ((Rs1D_i_dh==RdE_i_eh) | (Rs2D_i_dh==RdE_i_eh));
+assign StallF_o = should_we_stall;
+assign StallD_o = should_we_stall;
+//======================================
+
+//              FLUSH
+//======================================
+logic       PCSrcE;
+
+always_comb begin
+    PCSrcE = PCSrcE_i_eh[0] | PCSrcE_i_eh[1];
+    FlushD_o = PCSrcE;
+    FlushE_o = should_we_stall | PCSrcE;
+end
+//======================================
 
 endmodule
