c4b376666e2a2a78ce0fbff55d0e69bc78ce84a7
DenizzG <denizgoy2003@gmail.com>
2023-12-11 15:54:58 +0000

    completed forwarding

--- rtl/VERSION-3-HAZARDDETECTION/Hazard.sv
+++ rtl/VERSION-3-HAZARDDETECTION/Hazard.sv
@@ -21,15 +21,32 @@ module Hazard(
 );
 
 //           FORWARDING
+//===================================
+always_comb begin
+    if ((Rs1E_i_eh == RdM_i_mh) & RegWriteM_i_mh) & (Rs1E_i_eh != 0)
+        ForwardAE_o = 2'b10;
+    else if ((Rs1E_i_eh == RdW_i_wh) & RegWriteW_i_wh) & (Rs1E_i_eh != 0)
+        ForwardAE_o = 2'b01;
+    else 
+        ForwardAE_o = 2'b00;  
 
+    if ((Rs2E_i_eh == RdM_i_mh) & RegWriteM_i_mh) & (Rs2E_i_eh != 0)
+        ForwardBE_o = 2'b10;
+    else if ((Rs2E_i_eh == RdW_i_wh) & RegWriteW_i_wh) & (Rs2E_i_eh != 0)
+        ForwardBE_o = 2'b01;
+    else 
+        ForwardBE_o = 2'b00;  
+end
+//===================================
 
 //              STALL
 //====================================
 logic       should_we_stall;
-
-assign should_we_stall = ResultSrcE_i_eh & ((Rs1D_i_dh==RdE_i_eh) | (Rs2D_i_dh==RdE_i_eh));
-assign StallF_o = should_we_stall;
-assign StallD_o = should_we_stall;
+always_comb begin
+    should_we_stall = ResultSrcE_i_eh & ((Rs1D_i_dh==RdE_i_eh) | (Rs2D_i_dh==RdE_i_eh));
+    StallF_o = should_we_stall;
+    StallD_o = should_we_stall;
+end
 //======================================
 
 //              FLUSH
