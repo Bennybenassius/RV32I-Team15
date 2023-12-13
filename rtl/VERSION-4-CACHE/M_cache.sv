ffa0538c72fdec15a74e7835cee10014deca37a6
Yueming <rrroooyyywang@outlook.com>
2023-12-13 12:01:27 +0800

    Add New block for pipelining cpu, it replace M.sv block in top.sv
    - It wire up Data_mem and cache
    - add port StallAllM_o in addition to the old M.sv
    - StallAllM_o is to control tell all pipelining register to stall and waite the memory data is store correctly or cache miss is handled.

---
+++ rtl/VERSION-4-CACHE/M_cache.sv
@@ -0,0 +1,47 @@
+module M_cache(
+    //INPUTS
+    input logic           clk,
+    input logic [2: 0]    MemWriteM_i,
+    input logic [31:0]    ALUResultM_i,
+    input logic [31:0]    WriteDataM_i,
+
+    //OUTPUTS
+    output logic [31:0]   ReadDataM_o,
+    output logic StallAllM_o
+
+);
+//=======================================
+logic cache_hit;
+logic mem_ready;
+logic [31: 0] ReadData2Cache;
+
+Data_mem Data_mem(
+    //INPUTS
+    .clk(clk),
+    .WE(MemWriteM_i),
+    .A(ALUResultM_i),
+    .WD(WriteDataM_i),
+    .cache_hit(cache_hit),
+
+    //OUTPUTS
+    .RD(ReadData2Cache)
+    .mem_ready(mem_array)
+);
+
+cache cache(
+    //INPUTS
+    .clk(clk),
+    .WE(MemWriteM_i),
+    .A(ALUResultM_i),
+    .WriteDataCache_i(ReadData2Cache),
+
+    //OUTPUT
+    .cache_hit(cache_hit),
+    .ReadDataCache_o(ReadDataM_o)
+);
+
+always_comb begin
+    StallAllM_o = ~ (mem_ready & cache_hit);
+end
+
+endmodule
