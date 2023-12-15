055d4589b712fe341e8f1fbc07e884ed9bd828a6
Yueming <rrroooyyywang@outlook.com>
2023-12-15 23:10:01 +0800

    Modified code for plaground and add usage in README.md

--- test/playground/README.md
+++ test/playground/README.md
@@ -1,15 +1,17 @@
-## Usage of test_toolkit
-### New Usage
-- copy and paste [./test_toolkit](../test_toolkit/) itself and rename it. This will be the testing folder you are working on.
+## Usage of playground
+In command line type:
+```
+python3 testit.py --version=<version_to_test> --assembly=<assembly_code_to_test> --data=<data_to_use>
+```
 
-- `cd` into the folder you copy and renamed(your working folder), then copy and paste whole `rtl` folder into the testing folder.
+- `<version_to_test>`       can be: `1`, `2`, `3` and `4`
+- `<assembly_code_to_test>` can be `F1.s`, `F1_pipeline.s`, `pdf.s` and `pdf_pipeline.s`
+- `<data_to_use>` can be: `gaussian.mem`, `noisy.mem`, `triangle.mem`
 
-- write or modified your assembly code in file [test.s](test.s) in your working folder.
-- run command `python testit.py --version=1` for singlecycle
-- run command `python testit.py --version=2` for singlecycle
-- or if you rename your test assembly flle in your working folder, run `python testit.py --version=1 --assembly=<your_file_name>`(do not include '<' and '>)
-### Different from last usage
-- we do not copy and past all file in [./test_toolkit](../test_toolkit/) into rtl, instead we copy rtl folder into the [./test_toolkit](../test_toolkit/) (you should rename it) 
-- the new `testit.py` will now looking for keyword **version** and **assembly** to specify which version you want to test and which assembly code you want to use.
-- new version works on wsl
-
+Examples:
+```
+python3 testit.py --version=3 --assembly=F1.s
+```
+```
+python3 testit.py --version=2 --assembly=pdf_pipeline.s --data=gaussian.mem
+```
--- test/playground/testit.py
+++ test/playground/testit.py
@@ -1,35 +1,73 @@
+#This is the version for plaground only, not the same as actual test one
+
 import os
 import sys
 import string
 
 args = sys.argv
 #=============================parse_argument===========================================
-versions = ['VERSION-1-SINGLECYCLE', 'VERSION-2-PIPELINING', 'VERSION-3-HAZARDDETECTION', 'VERSION-4-CACHE']
-
-version = 'VERSION-1-SINGLECYCLE'
-assembly = 'test.s'
+versions = ['VERSION-1-F1', 'VERSION-1-reference', 'VERSION-2-F1', 'VERSION-2-reference', 'VERSION-3-F1', 'VERSION-3-reference', 'VERSION-4-F1', 'VERSION-4-reference',]
+_versions = []
+version_num = 1
+version = 'VERSION-1-F1'
+assembly = 'F1.s'
+data = 'empty.mem'
 
 for arg in args:
     if 'version' in arg:
-        version = versions[int(arg.partition('=')[2]) - 1]
+        version_num = str(int(arg.partition('=')[2]))
     if 'assembly' in arg:
         assembly = arg.partition('=')[2]
+    if 'data' in arg:
+        data = arg.partition('=')[2]
+
+for _version in versions:
+    if version_num == _version.partition('-')[2][0]:
+        _versions += [_version]
+
+for __version in _versions:
+    if assembly == 'F1_pipeline.s':
+        version = f'VERSION-{version_num}-F1'
+        break
+    if assembly == 'pdf_pipeline.s':
+        version = f'VERSION-{version_num}-reference'
+        break
+    
+    if assembly == 'F1.s':
+        if 'F1' in __version:
+            version = __version
+            break
+    if assembly == 'pdf.s':
+        if 'reference' in __version:
+            version = __version
+            break
+
 
 src_raw = f'./{assembly}'
 dst_raw = './src/myprog'
+
+src_mem = f'./{data}'
+dst_mem = f'./rtl/{version}/Data.mem'
 
 src_hex = f'./src/myprog/{assembly}.hex'
 dst_hex = f'./rtl/{version}/instruction_code.mem'
 
 dst_rtl = f'./rtl/{version}/'
+
 #=============================end_parse_argument=======================================
 
 if os.name == 'nt':  # Windows
     cmd = 'copy ' + src_raw + " " + dst_raw
 else:  # Unix/Linux
     cmd = 'cp ' + src_raw + " " + dst_raw
+    
+if os.name == 'nt':  # Windows
+    cmd_mem = 'copy ' + src_mem + " " + dst_mem
+else:  # Unix/Linux
+    cmd_mem = 'cp ' + src_mem + " " + dst_mem
 os.system('echo "==================Copy_.s_file===================="')
 os.system(cmd)
+os.system(cmd_mem)
 
 os.system('echo "==================Make_hex_file==================="')
 os.system("cd ./src \n make hexfile")
